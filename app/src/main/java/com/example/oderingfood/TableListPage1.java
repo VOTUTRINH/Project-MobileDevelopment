42ae53cbf4378282cf88b2527365be7fae0e8928
20120597 <toainguyenvan20102002@gmail.com>
2022-10-31 15:56:53 +0700

    Toai da lam xong phan danh sach ban an

---
+++ app/src/main/java/com/example/oderingfood/TableListPage1.java
@@ -0,0 +1,84 @@
+package com.example.oderingfood;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TableListPage1#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TableListPage1 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    TablesActivity tablesActivity;
+    Context context;
+    GridView gv;
+    String[] arrayName = {"1","2","3","4","5","6","7","8","9","10","11","12"};
+
+    public TableListPage1() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment TableListPage1.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TableListPage1 newInstance(String param1, String param2) {
+        TableListPage1 fragment = new TableListPage1();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        try {
+            context = getActivity();
+            tablesActivity = (TablesActivity)getActivity();
+        }catch (Exception e)
+        {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        LinearLayout layout_page1 =(LinearLayout)inflater.inflate(R.layout.fragment_table_list_page1,null);
+
+        gv = (GridView) layout_page1.findViewById(R.id.grid_view);
+        ListTablesAdapter tablesAdapter = new ListTablesAdapter(context,R.layout.table_layout_item,arrayName);
+        gv.setAdapter(tablesAdapter);
+
+        return layout_page1;
+    }
+}
---
+++ app/src/main/java/com/example/oderingfood/TablesActivity.java
@@ -0,0 +1,101 @@
+package com.example.oderingfood;
+
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class TablesActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FloatingActionButton btnAddTable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tables);
+
+
+        viewPager2 = (ViewPager2) findViewById(R.id.view_pager_list_table);
+        tabLayout = (TabLayout) findViewById(R.id.tab_list_table);
+        btnAddTable = (FloatingActionButton) findViewById(R.id.add_table_button);
+
+        ListTableViewPagerAdapter adapter = new ListTableViewPagerAdapter(this);
+
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        switch (position)
+                        {
+                            case 0:
+                            {
+                                tab.setText("Tất cả");
+                                break;
+                            }
+                            case 1:
+                            {
+                                tab.setText("Sử dụng");
+                                break;
+                            }
+                            case 2:
+                            {
+                                tab.setText("Còn trống");
+                                break;
+                            }
+                            default: tab.setText("Quên chưa case nè");
+                        }
+                    }
+                }).attach();
+
+        btnAddTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowDialogAddTable();
+            }
+        });
+    }
+
+    private void ShowDialogAddTable()
+    {
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.add_table_layout);
+        dialog.show();
+
+        Button btn_cancel = (Button) dialog.findViewById(R.id.btn_cancel);
+        Button btn_accept = (Button) dialog.findViewById(R.id.btn_accept);
+
+        btn_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        btn_accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(TablesActivity.this, "Accept", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+    }
+}
