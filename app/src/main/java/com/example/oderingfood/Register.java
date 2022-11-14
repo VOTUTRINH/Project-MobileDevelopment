7c12049f31f258e6cd1db6546c370b2eec60e1f0
20120352 <phuc9a134@gmail.com>
2022-11-15 00:29:22 +0700

    add photo

--- app/src/main/java/com/example/oderingfood/Register.java
+++ app/src/main/java/com/example/oderingfood/Register.java
@@ -2,12 +2,13 @@ package com.example.oderingfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Register extends AppCompatActivity {
+public class Register extends Activity {
 
     Button button1;
     Button button2;
--- app/src/main/java/com/example/oderingfood/Register2.java
+++ app/src/main/java/com/example/oderingfood/Register2.java
@@ -1,19 +1,35 @@
 package com.example.oderingfood;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class Register2 extends AppCompatActivity {
-
+public class Register2 extends Activity {
+    ImageView add_image1;
     Button btn_submit;
+    int SELECT_IMAGE_CODE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        add_image1=(ImageView)findViewById(R.id.add_img);
+        add_image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"title"),SELECT_IMAGE_CODE);
+            }
+        });
+
         btn_submit = findViewById(R.id.submit);
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,5 +39,15 @@ public class Register2 extends AppCompatActivity {
         });
 
 
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1) {
+            Uri uri = data.getData();
+            add_image1.setImageURI(uri);
+        }
     }
 }
