d254c451434b295bc523b07989a49ad1229ea56f
Vo Tu Trinh <votutrinh2002@gmail.com>
2022-11-19 17:10:32 +0700

    Trinh push

--- app/src/main/java/com/example/oderingfood/Register.java
+++ app/src/main/java/com/example/oderingfood/Register.java
@@ -1,27 +1,61 @@
 package com.example.oderingfood;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.SignInMethodQueryResult;
 
 public class Register extends Activity {
 
-    Button button1;
+    Button button_continue;
     Button button2;
+    EditText email,password,confirm_pass;
+    CheckBox agree;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        button1=(Button) findViewById(R.id.btncontinue);
-        button1.setOnClickListener(new View.OnClickListener() {
+
+        button_continue=(Button) findViewById(R.id.btncontinue);
+        button2 =(Button) findViewById(R.id.btnlogin);
+        email =(EditText) findViewById(R.id.signup_email);
+        password=(EditText) findViewById(R.id.signup_pass);
+        confirm_pass =(EditText) findViewById(R.id.confirm_pass);
+        agree =(CheckBox) findViewById(R.id.checkbox_meat);
+
+        auth =FirebaseAuth.getInstance();
+
+
+        button_continue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(Register.this,Register2.class);
-                startActivity(intent);
+                String text_email = email.getText().toString();
+                String text_pass = password.getText().toString();
+                String text_confirm = confirm_pass.getText().toString();
+                if(text_email.isEmpty() || text_pass.isEmpty() || text_confirm.isEmpty()){
+                    Toast.makeText(Register.this, "Thông tin chưa đầy đủ.", Toast.LENGTH_SHORT).show();
+                }else if(!text_confirm.equals(text_pass)){
+                    Toast.makeText(Register.this, "Mật khẩu không trùng khớp.", Toast.LENGTH_SHORT).show();
+                }else{
+                    register(text_email,text_pass);
+                }
+
             }
         });
         button2=(Button) findViewById(R.id.btnlogin);
@@ -32,5 +66,36 @@ public class Register extends Activity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void register(String email, String pass) {
+        auth.fetchSignInMethodsForEmail(email)
+            .addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                @Override
+                public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+
+                    boolean isNewUser = task.getResult().getSignInMethods().isEmpty();
+
+                    if (isNewUser) {
+                        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(Register.this, "Đăng kí thành công.", Toast.LENGTH_SHORT).show();
+                                    Intent intent=new Intent(Register.this,Register2.class);
+                                    startActivity(intent);
+                                    finish();
+                                }else {
+                                    Toast.makeText(Register.this, "Đăng kí thất bại.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    } else {
+                        Toast.makeText(Register.this, "Email đã tồn tại.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            });
+
     }
 }
