754c9455197c33dd9ca6add8f5d5644a06cac2d2
heoquaycutekhung <chinhnguyen140302@gmail.com>
2022-11-22 10:07:44 +0700

    nothing

--- app/src/main/java/com/example/oderingfood/ListTablesAdapter.java
+++ app/src/main/java/com/example/oderingfood/ListTablesAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -38,6 +39,24 @@ public class ListTablesAdapter extends ArrayAdapter<String> {
             public void onClick(View view) {
                 PopupMenu popupMenu = new PopupMenu((Activity)context,btnMoreAction);
                 popupMenu.getMenuInflater().inflate(R.menu.menu_table_action, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu_thanhtoan:
+                                //handle menu1 click
+                                return true;
+                            case R.id.menu_xoabanan:
+                                //handle menu2 click
+                                Intent intent = new Intent(context, A2G7Activity.class);
+                                context.startActivity(intent);
+                                return true;
+
+                            default:
+                                return false;
+                        }
+                    }
+                });
                 popupMenu.show();
             }
         });
