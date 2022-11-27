2cb2a63a439f65151103177a8e073f76afff34d7
20120352 <phuc9a134@gmail.com>
2022-11-28 00:45:53 +0700

    update fragment

--- app/src/main/java/com/example/oderingfood/TuyChon_Fragment.java
+++ app/src/main/java/com/example/oderingfood/TuyChon_Fragment.java
@@ -7,10 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class TuyChon_Fragment extends Fragment {
-
+    Button button1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
